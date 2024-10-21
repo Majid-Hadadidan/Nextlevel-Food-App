80dd569dc306e76e596c89a4173c37f7a90616aa
majidH <majid.hadadian@gmail.com>
2024-10-21 18:50:57 +0330

    add Meals before create database sqlite3

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,15 +1,24 @@
 import Link from "next/link";
-
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 export default function MealsPage() {
   return (
-    <main>
-      <h1>MealsPage</h1>
-      <h2>
-        <Link href="/meals/meals-1">meals 1</Link>
-      </h2>
-      <h2>
-        <Link href="/meals/meals-2">meals 2</Link>
-      </h2>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals,created{" "}
+          <span className={classes.heighlight}>by you </span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself .it is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]}/>
+      </main>
+    </>
   );
 }
